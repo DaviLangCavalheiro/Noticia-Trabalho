a2066d62d8ad58557d710787f1a0cae94f02d513
Davi Lang Cavalheiro <cavalheiro.davi@escola.pr.gov.br>
2026-08-12 10:39:37 -0300

    mudanças

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+let curtidas = 0;
+
+const botaoCurtir = document.getElementById('btnCurtir');
+const contador = document.getElementById('contador');
+
+botaoCurtir.addEventListener('click', () => {
+    curtidas++;
+    contador.textContent = curtidas;
+});
